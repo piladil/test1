5169b7df8165850f21a7244cfa58d5314fd3d84d
dil <dil@crossovernepal.com>
2015-01-14 05:26:41 -0500

    added

---
+++ projects/krishna/web/index.php
@@ -0,0 +1,24 @@
+this is krishna project
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo  "this is the project";
+     $t = date("H");
+
+    if ($t <  "20") {
+     echo "<br>Have a good day!<br> nice day!";
+}
+        ?>
+    </body>
+</html>
+
